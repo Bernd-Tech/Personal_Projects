6ebf4229c85bb095fe0226b8aad0ddd1dc8dc157
Bernd Pierre Louis <berndpierrelouis@icloud.com>
2025-06-28 01:04:02 +0200

    crypto chart still work in progress
    git status

--- cryptoChart/cryptoChart.js
+++ cryptoChart/cryptoChart.js
@@ -17,3 +17,70 @@
 // TASK 9: Display a loading spinner during the data fetching process, removing it when data arrives.
 
 // TASK 10: Set an interval to automatically refresh and update the charts every 10 seconds.
+
+
+const loader = document.getElementById("loader-container");
+const chartSection = document.getElementById("chart-section");
+
+const api = axios.create({
+    baseURL: "https://api.coinbase.com/v2/assets/prices"
+})
+
+const coins = ["bitcoin", "ethereum"];
+
+const fetchData = async () => {
+    loader.style.display = "flex";
+
+    try {
+        const responses = await Promise.all(
+            coins.map(async (coin) => {
+                const response = await api.get(`${coin}`);
+                const prices = response.data.data.prices.hour.prices.slice(0, 24);
+                // console.log(prices)
+
+                const labels = prices.map(([, timestamp]) => 
+                    new Date(timestamp * 1000).toLocaleTimeString()
+                )
+
+                const data = prices.map(([price]) => Number(price))
+                return {coinId: coin, labels, data, symbol: response.data.data.base}
+            })
+        )
+        loader.style.display = "none";
+
+        responses.forEach((res) => {
+
+            const chart = createChart(res.coinId, res.labels, res.data, res.symbol);
+            
+        })
+        console.log(responses)
+    } catch (error) {
+        loader.style.display = "none";
+        console.error(error, ": Data could not be fetched.")
+        chartSection.innerText = error + ": Data could not be fetched.";
+    }
+}
+
+const createChart = (coinId, labels, data, symbol) => {
+    const canvas = document.createElement("canvas");
+    canvas.id = coinId;
+
+    new Chart(canvas, {
+        type: "line",
+        data: {
+            labels,
+            datasets: [{
+                label: `${symbol} hourly prices`,
+                data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            fill: true,
+        }
+    })
+    chartSection.appendChild(canvas);
+}
+
+
+fetchData()
